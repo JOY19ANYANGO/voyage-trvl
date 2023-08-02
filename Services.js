388d5acc386ebaf96362f44ef9fc413dc166431e
samuel <samuel.mbula@student.moringaschool.com>
2023-08-02 22:53:53 +0300

    voyage-travel app

--- Services.js
+++ Services.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-import Transport from './MeansOfTransport';
-import PaymentSystem from './PaymentSystem';
+import Transport from './src/Services/MeansOfTransport';
+import PaymentSystem from './src/Services/PaymentSystem';
 
 const App = () => {
-  const TransportOptions = [
+  const sampleTransportOptions = [
     { id: 1, title: 'Car', description: 'Travel by car.' },
     { id: 2, title: 'Train', description: 'Travel by train.' },
     { id: 3, title: 'Bus', description: 'Travel by bus.' },
+    { id: 3, title: 'Plain', description: 'Travel by Plain.' },
   ];
 
   const samplePaymentOptions = [
     { id: 'paypal', title: 'PayPal', description: 'Pay with PayPal.' },
     { id: 'credit_card', title: 'Credit Card', description: 'Pay with Credit Card.' },
     { id: 'bank_transfer', title: 'Bank Transfer', description: 'Pay with Bank Transfer.' },
+    { id: 'M-PESA', title: 'M-PESA', description: 'Pay with M-PESA.' },
+    { id: 'Airtel Money', title: 'Airtel Money', description: 'Pay with Airtel Money.' },
   ];
 
   const [selectedTransport, setSelectedTransport] = useState(null);
@@ -28,7 +31,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Services</h1>
+      <h1>OUR SERVICES</h1>
 
       <Transport transportOptions={sampleTransportOptions} handleSelectTransport={handleSelectTransport} />
 
