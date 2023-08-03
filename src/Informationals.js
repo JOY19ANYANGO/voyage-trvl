067ba5bef2b6b0dac5e4dcab722f0526aa88a118
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-08-03 09:37:01 +0300

    Informationals

---
+++ src/Informationals.js
@@ -0,0 +1,18 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Contact from "./Contact";
+import AboutUs from "./About Us";
+import PrivacyPolicy from "./Privacy policy";
+import TermsAndConditions from "./Terms and conditions";
+function Informationals() {
+  return (
+   <>
+   <Contact/>
+   <AboutUs/>
+   <PrivacyPolicy/>
+   <TermsAndConditions/>
+   </>
+  );
+}
+
+export default Informationals;
