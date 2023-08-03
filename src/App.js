b06b9be2275f3c10523179817a58d7e78c9fdb2e
Abdulaziz <abdulaziz.komara@student.moringaschool.com>
2023-08-03 15:20:47 +0300

    resoleved issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import InformationalsContact from "./InformationalsContact";
-import InformationalsAboutUs from "./InformationalsAboutUs";
-import InformationalsPrivacypolicy from "./InformationalsPrivacyPolicy";
-import InformationalsTermsAndConditions from "./InformationalsTerms and conditions";
+import NavBar from "./NavBar";
+import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
+import Home from "./Home";
+import Contacts from "./Contacts";
+import AboutUs from "./AboutUs";
+import Reviews from "./Reviews";
+import Places from "./Places";
+import AboutUs from "./InformationalsAboutUs"
+import Contact from "./InformationalsContactContact"
+import PrivacyPolicy from "./Privacy policy";
+import TermsAndConditions from "./Terms and conditions";
 
 function App() {
   return (
-   <>
-   <InformationalsContact/>
-   <InformationalsAboutUs/>
-   <InformationalsPrivacypolicy/>
-   <InformationalsTermsAndConditions/>
-   </>
+    <>
+    <Router>
+      <NavBar/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/AboutUs"  component={AboutUs} />
+        <Route path="/Reviews"  component={Reviews} />
+        <Route path="/Places"  component={Places} />
+        <Route path="/Contact"  component={Contact} />
+        <Route path="/Privacypolicy" component={PrivacyPolicy}/>
+        <Route path="/Termsandconditions" component={TermsAndConditions}/>
+      </Switch>
+    </Router>
+    </>
   );
 }
 
-export default App;
+export default App
